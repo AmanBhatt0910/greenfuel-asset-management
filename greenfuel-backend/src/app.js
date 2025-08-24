5e6f86e657ec1e24beb3aa1702eadc48262090fd
Aman Bhatt <amanbhatt0910@gmail.com>
2025-08-24 12:11:31 +0530

    Auth Routes added

--- greenfuel-backend/src/app.js
+++ greenfuel-backend/src/app.js
@@ -3,12 +3,16 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./utils/db');
 const Admin = require('./models/Admin');
+const adminRoutes = require('./routes/admin');
+const auth = require('./middleware/auth');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/admin', adminRoutes);
 
 sequelize.authenticate()
   .then(() => console.log('Database connected...'))
